dfe182cf884d0bd481f5bd14b4ebd3fcb3780237
Lucas Maciel Messias <lucas.messias@solvs.corp>
2020-03-10 17:19:26 -0300

    Back-end

---
+++ Fase02/Módulo02/Back-end/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+
+server.use(express.static('public'))
+
+server.set("view engine", "html")
+
+nunjucks.configure("views", {
+    express: server
+})
+
+server.get("/", function(req, res) {
+    return res.render("index")
+})
+
+server.listen(6000, function() {
+    console.log("Server is running")
+})
+
